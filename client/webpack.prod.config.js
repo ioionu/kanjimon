da8a058c666a237843fa0d0ce520a566f63cd4f4
josh <io@ioio.nu>
2017-12-02 22:27:12 +1100

    prod build

---
+++ client/webpack.prod.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+var webpack = require('webpack')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const dist = 'js'
+
+const config = {
+  entry: './src/script.js',
+  output: {
+    path: path.resolve(__dirname, 'js'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader','css-loader']
+      },
+      {
+        test: /\.(svg)$/,
+        loader: 'svg-url-loader?classPrefix'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
+    new CopyWebpackPlugin([
+      {from: 'db/kanjidic2.json', to: 'db.json'},
+      {from: 'node_modules/sw-toolbox/sw-toolbox.js', to: 'sw-toolbox.js'},
+      {from: 'src/serviceWorker.js', to: 'serviceWorker.js'},
+    ]),
+  ]
+};
+
+module.exports = config
